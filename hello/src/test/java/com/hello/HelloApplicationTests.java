4aca9a2301c3a712db8836cd0848d6af13a7d521
drm <dmuzik00@gmail.com>
2015-12-23 17:18:21 +0000

    0

--- hello/src/test/java/com/hello/HelloApplicationTests.java
+++ hello/src/test/java/com/hello/HelloApplicationTests.java
@@ -10,7 +10,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 public class HelloApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void contextLoads() { 
 	}
 
 }
